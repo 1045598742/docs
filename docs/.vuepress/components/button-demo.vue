db642261278aa44384b6e028d04ad5828c9fb5fa
libo <1045598742@qq.com>
2020-05-16 17:00:30 +0800

    清理无用的样式

--- docs/.vuepress/components/button-demo.vue
+++ docs/.vuepress/components/button-demo.vue
@@ -50,14 +50,3 @@ export default {
   }
 };
 </script>
-<style lang="scss">
-.swiper-item_demo {
-  line-height: 300px;
-  text-align: center;
-  color: white;
-  font-size: 20px;
-}
-.swiper-item_demo {
-  background: #3eaf7c;
-}
-</style>
