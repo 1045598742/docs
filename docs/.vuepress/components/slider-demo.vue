be7998e05a51237ad937519bac7aab01ed1f6d0b
libo <1045598742@qq.com>
2020-05-31 20:40:41 +0800

    新增图片裁切组件，优化部分demo体验

--- docs/.vuepress/components/slider-demo.vue
+++ docs/.vuepress/components/slider-demo.vue
@@ -8,7 +8,7 @@
       </div>
       <lb-slider lang="300px" v-model="value"/>
     </demo>
-    <demo :code="code1">
+    <demo code="">
       <template #title>基础用法</template>
       <template #subtitle>可设置横向和纵向滑块，因文档原因demo会卡顿，项目运行时是正常的</template>
       <a href="/docs/music.html"><lb-button type="success">点击查看本组件在音乐播放器demo的应用</lb-button></a>
